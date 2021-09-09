643fd4d5865cac2ec8304c84e77b462c99825ec0
mgrygles <mgrygles@gmail.com>
2021-09-09 01:45:01 -0500

    Initial commit

---
+++ scripts/packageApps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mvn -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    -pl models clean install
+mvn -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    -q clean package
